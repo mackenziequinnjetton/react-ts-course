20e921cfcc620e7b188489cfa00d17143a1c5df7
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-07-21 16:21:43 -0600

    Move first app into subdirectory

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const buttonText = "Click me!"
+
   return (
     <div>
-      <label for="name" class="label">enter email</label>
+      <label htmlFor="name" className="label">enter email</label>
       <input id="name" type="text" />
-      <button style="background-color: red; color: white;">Submit</button>
+      <button style={{backgroundColor: "red", color: "white"}}>
+        {buttonText }
+      </button>
     </div>
   )
 }
