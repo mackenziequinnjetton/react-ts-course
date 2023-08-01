11351414434efce9a7366841bf735667e6d7b463
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-01 09:03:45 -0600

    Update context-api project

--- context-api/src/components/TodoList.jsx
+++ context-api/src/components/TodoList.jsx
@@ -1,35 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class TodoList extends React.Component {
-  static contextType = ThemeContext;
+const TodoList = () => {
+  const { isDarkTheme, lightTheme, darkTheme, changeTheme } = useContext(ThemeContext);
+  const theme = isDarkTheme ? darkTheme : lightTheme;
 
-  render() {
-    const { isDarkTheme, lightTheme, darkTheme, changeTheme } = this.context;
-    const theme = isDarkTheme ? darkTheme : lightTheme;
-    return (
-      <div 
-        style = {{
-          background: theme.background,
-          color: theme.text,
-          height: "140px",
-          textAlign: "center"
-        }} 
-        className="ui list"
-      >
-        <p className="item">
-          Plan the family trip
-        </p>
-        <p className="item">
-          Go shopping for dinner
-        </p>
-        <p className="item">
-          Go for a walk
-        </p>
-        <button className="ui button primary" onClick={changeTheme}>Change the theme</button>
-      </div>
-    )
-  }
+  return (
+    <div 
+      style = {{
+        background: theme.background,
+        color: theme.text,
+        height: "140px",
+        textAlign: "center"
+      }} 
+      className="ui list"
+    >
+      <p className="item">
+        Plan the family trip
+      </p>
+      <p className="item">
+        Go shopping for dinner
+      </p>
+      <p className="item">
+        Go for a walk
+      </p>
+      <button className="ui button primary" onClick={changeTheme}>Change the theme</button>
+    </div>
+  );
 }
 
 export default TodoList;
